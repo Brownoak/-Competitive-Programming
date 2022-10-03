e62b9d9cf11bb2791a631fb14ac523e4c5b9a5dd
Brownoak <helanii93@gmail.com>
2022-10-02 23:42:44 -0700

    leet code solved problem 2

---
+++ add_Two_Numbers.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        curr= temp = ListNode(0)
+        holder = 0
+
+        while holder or l1 or l2:
+          if l1:
+            holder += l1.val
+            l1 = l1.next
+          if l2:
+            holder += l2.val
+            l2 = l2.next
+          curr.next = ListNode(holder % 10)
+          holder //= 10
+          curr = curr.next
+
+        return temp.next
+        
