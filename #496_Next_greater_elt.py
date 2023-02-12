ab7e88fbe9cbcab49b968844f291bb224da6047d
Brownoak <helanii93@gmail.com>
2023-02-12 20:38:17 +0300

    leet code 496

---
+++ #496_Next_greater_elt.py
@@ -0,0 +1,9 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        great_element = [-1] * len(nums1)
+        for num in nums1:
+            for _great in nums2[nums2.index(num):]:
+                if _great > num:
+                    great_element[nums1.index(num)] = _great
+                    break
+        return great_element
